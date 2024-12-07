749f281ecf22297db881fba0d6f8abd246a92036
sean <seanl20@yahoo.com>
2024-12-07 11:16:22 +1000

    feat: learn j3Kanban

--- app/javascript/controllers/board_controller.js
+++ app/javascript/controllers/board_controller.js
@@ -2,27 +2,89 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
+    const boards = [
+      {
+        "id"    : "board-id-1",
+        "title" : "Board Title 1",
+        "class" : "bg-red-700, text-white",
+        "item"  : [
+          {
+            "id"      : "item-id-1",
+            "title"   : "Item 1",
+            "username": "username1"
+          },
+          {
+            "id"      : "item-id-2",
+            "title"   : "Item 2",
+            "username": "username2"
+          }
+        ]
+      },{
+        "id"    : "board-id-1",
+        "title" : "Board Title 2",
+        "class" : "bg-blue-700, text-white",
+        "item"  : [
+          {
+            "id"      : "item-id-1",
+            "title"   : "Item 1",
+            "username": "username1"
+          },
+          {
+            "id"      : "item-id-2",
+            "title"   : "Item 2",
+            "username": "username2"
+          }
+        ]
+      },{
+        "id"    : "board-id-1",
+        "title" : "Board Title 3",
+        "class" : "bg-green-700, text-white",
+        "item"  : [
+          {
+            "id"      : "item-id-1",
+            "title"   : "Item 1",
+            "username": "username1"
+          },
+          {
+            "id"      : "item-id-2",
+            "title"   : "Item 2",
+            "username": "username2"
+          }
+        ]
+      },{
+        "id"    : "board-id-1",
+        "title" : "Board Title 4",
+        "item"  : [
+          {
+            "id"      : "item-id-1",
+            "title"   : "Item 1",
+            "username": "username1"
+          },
+          {
+            "id"      : "item-id-2",
+            "title"   : "Item 2",
+            "username": "username2"
+          }
+        ]
+      }
+    ]
+
     var kanban = new jKanban({
-      element          : '#board',                                           // selector of the kanban container
-      gutter           : '15px',                                       // gutter of the board
-      widthBoard       : '250px',                                      // width of the board
-      responsivePercentage: false,                                    // if it is true I use percentage in the width of the boards and it is not necessary gutter and widthBoard
-      dragItems        : true,                                         // if false, all items are not draggable
-      boards           : [],                                           // json of boards
-      dragBoards       : true,                                         // the boards are draggable, if false only item can be dragged
+      element          : '#board',                                   // selector of the kanban container
+      boards           : boards,                                         // json of boards
       itemAddOptions: {
-          enabled: false,                                              // add a button to board for easy item creation
-          content: '+',                                                // text or html content of the board button   
-          class: 'kanban-title-button btn btn-default btn-xs',         // default class of the button
-          footer: false                                                // position the button on footer
-      },    
+        enabled: false,                                              // add a button to board for easy item creation
+        content: '+',                                                // text or html content of the board button   
+        class: 'kanban-title-button btn btn-default btn-xs',         // default class of the button
+        footer: false                                                // position the button on footer
+      },
       itemHandleOptions: {
-          enabled             : false,                                 // if board item handle is enabled or not
-          handleClass         : "item_handle",                         // css class for your custom item handle
-          customCssHandler    : "drag_handler",                        // when customHandler is undefined, jKanban will use this property to set main handler class
-          customCssIconHandler: "drag_handler_icon",                   // when customHandler is undefined, jKanban will use this property to set main icon handler class. If you want, you can use font icon libraries here
-          customHandler       : "<span class='item_handle'>+</span> %title% "  // your entirely customized handler. Use %title% to position item title 
-                                                                              // any key's value included in item collection can be replaced with %key%
+        enabled             : false,                                 // if board item handle is enabled or not
+        handleClass         : "item_handle",                         // css class for your custom item handle
+        customCssHandler    : "drag_handler",                        // when customHandler is undefined, jKanban will use this property to set main handler class
+        customCssIconHandler: "drag_handler_icon",                   // when customHandler is undefined, jKanban will use this property to set main icon handler class. If you want, you can use font icon libraries here
+        customHandler       : "<span class='item_handle'>+</span> %title% " // your entirely customized handler. Use %title% to position item title 
+                                                                            // any key's value included in item collection can be replaced with %key%
       },
       click            : function (el) {},                             // callback when any board's item are clicked
       context          : function (el, event) {},                      // callback when any board's item are right clicked
@@ -34,6 +96,6 @@ export default class extends Controller {
       buttonClick      : function(el, boardId) {},                     // callback when the board's button is clicked
       propagationHandlers: [],                                         // the specified callback does not cancel the browser event. possible values: "click", "context"
     });
-    console.log(kanban);
+    console.log('Kanban: ', kanban);
   }
 }
