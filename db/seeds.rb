90c052ed8fc7f29c9a445ccc44e33eac990c3f0c
sean <seanl20@yahoo.com>
2024-12-09 15:30:22 +1000

    feat: add api call for lists

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,21 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+user = User.create(email: "test@test.com", password: "password")
+
+5.times do |i|
+  Board.create(
+    user:,
+    name: "Board #{i + 1}"
+  )
+end
+
+Board.find_each do |board|
+  5.times { |i| List.create(board:, title: "List #{i + 1}") }
+
+  board.reload.lists.each do |list|
+    5.times { |i| Item.create(list:, title: "Item #{i + 1}") }
+  end
+end
