c30b87d00379c4169d776c25a9264c92a5998636
sean <seanl20@yahoo.com>
2024-12-12 15:38:38 +1000

    refactor: board js controller

--- app/javascript/controllers/board_controller.js
+++ app/javascript/controllers/board_controller.js
@@ -1,42 +1,14 @@
 import { Controller } from '@hotwired/stimulus'
 import axios from 'axios'
-import { get, map, isNull } from 'lodash-es'
+import { get, map, sample } from 'lodash-es'
 
 export default class extends Controller {
   HEADERS = { 'ACCEPT': 'application/json' }
+  BACKGROUND_COLORS = ['bg-green-700', 'bg-blue-700', 'bg-red-700', 'bg-slate-700', 'bg-yellow-700']
 
   connect() {
-    axios.get('/api/boards/1/lists', { header: this.HEADERS}).then((response) => {
-      console.log("Response: ", response);
-      const boards = this.buildBoards(response["data"]);
-
-      var kanban = new jKanban({
-        element          : '#board',                                   // selector of the kanban container
-        boards           : boards,                                         // json of boards
-        itemAddOptions: {
-          enabled: false,                                              // add a button to board for easy item creation
-          content: '+',                                                // text or html content of the board button   
-          class: 'kanban-title-button btn btn-default btn-xs',         // default class of the button
-          footer: false                                                // position the button on footer
-        },
-        itemHandleOptions: {
-          enabled             : false,                                 // if board item handle is enabled or not
-          handleClass         : "item_handle",                         // css class for your custom item handle
-          customCssHandler    : "drag_handler",                        // when customHandler is undefined, jKanban will use this property to set main handler class
-          customCssIconHandler: "drag_handler_icon",                   // when customHandler is undefined, jKanban will use this property to set main icon handler class. If you want, you can use font icon libraries here
-          customHandler       : "<span class='item_handle'>+</span> %title% " // your entirely customized handler. Use %title% to position item title 
-                                                                              // any key's value included in item collection can be replaced with %key%
-        },
-        click            : function (el) {},                             // callback when any board's item are clicked
-        context          : function (el, event) {},                      // callback when any board's item are right clicked
-        dragEl           : function (el, source) {},                     // callback when any board's item are dragged
-        dragendEl        : function (el) {},                             // callback when any board's item stop drag
-        dropEl           : function (el, target, source, sibling) {},    // callback when any board's item drop in a board
-        dragBoard        : function (el, source) {},                     // callback when any board stop drag
-        dragendBoard     : function (el) {},                             // callback when any board stop drag
-        buttonClick      : function(el, boardId) {},                     // callback when the board's button is clicked
-        propagationHandlers: [],                                         // the specified callback does not cancel the browser event. possible values: "click", "context"
-      });
+    axios.get(this.element.dataset.apiUrl, { header: this.HEADERS}).then((response) => {
+      this.buildKanban(this.buildBoards(response["data"]))
     });
   }
 
@@ -45,18 +17,14 @@ export default class extends Controller {
       return {
         'id': get(board, 'id'),
         'title': get(board, 'attributes.title'),
-        'class': this.buildClassList(get(board, 'attributes.class_list')),
+        'class': this.buildClassList(),
         'item': this.buildItems(get(board, 'attributes.items.data'))
       }
     });
   }
 
-  buildClassList(classList){
-    if (isNull(classList)) {
-      return ''
-    }
-
-    return classList.split(' ').join(', ')
+  buildClassList(){
+    return `text-white, ${sample(this.BACKGROUND_COLORS)}`
   }
 
   buildItems(items) {
@@ -64,12 +32,18 @@ export default class extends Controller {
       return {
         'id': get(item, 'id'),
         'title': get(item, 'attributes.title'),
-        'class': this.buildClassList(get(item, 'attributes.class_list'))
+        'class': this.buildClassList()
       }
     });
   }
 
-  buildKanban(){
-
+  buildKanban(boards){
+    new jKanban({
+      element: `#${this.element.id}`,
+      boards: boards,
+      itemAddOptions: {
+        enabled: true
+      }
+    });
   }
 }
